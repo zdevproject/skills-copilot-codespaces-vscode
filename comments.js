5e7b8b4bdf347ac8f66b8651311e5b567235d5b0
zdevproject <162572581+zdevproject@users.noreply.github.com>
2024-03-30 05:52:30 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+// Create web server
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, world!');
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server running at http://localhost:3000/');
+});
